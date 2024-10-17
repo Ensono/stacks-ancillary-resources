be7abc45c1982499a4d83e1fd35aee80a447e03c
Jack Blower <ElvenSpellmaker@users.noreply.github.com>
2024-10-17 13:26:48 +0100

    fix: tf lint

--- src/ancillary_resources/terraform/azure/key_vault.tf
+++ src/ancillary_resources/terraform/azure/key_vault.tf
@@ -16,8 +16,8 @@ resource "azurerm_key_vault" "kv" {
   tenant_id = data.azurerm_client_config.service_principal.tenant_id
 
   access_policy {
-    tenant_id      = data.azurerm_client_config.service_principal.tenant_id
-    object_id      = data.azurerm_client_config.service_principal.object_id
+    tenant_id = data.azurerm_client_config.service_principal.tenant_id
+    object_id = data.azurerm_client_config.service_principal.object_id
 
     secret_permissions      = var.key_vault_secret_perms
     certificate_permissions = var.key_vault_cert_perms
